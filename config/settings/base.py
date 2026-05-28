3ed64096e32c5231f284a93f3549ebb31b39416d
matsugeEX <matsuge0727@icloud.com>
2026-05-28 07:40:03 +0000

    create .env and setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,17 +11,18 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from timedelta import datetime
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+load_dotenv(BASE_DIR / ".env")
+#SECRET_KEY = os.getenv("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-csy5nk!g%z7y50!sae21gc&1#40jkx)s=z8m^_hz+3i5*tv_+)"
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- config/settings/development.py
+++ config/settings/development.py
@@ -1,11 +1,13 @@
 from .base import *
 
+DATABASS = os.getenv("DATABASS")
+
 DATABASES = {
     "default":{
         "ENGINE":"django.db.backends.mysql",
-        "NAME":"app",
-        "USER":"root",
-        "PASSWORD":"password",
+        "NAME":os.getenv("DB_NAME"),
+        "USER":os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST":"host.docker.internal",
         "PORT":"53306",
         "ATOMIC_REQUESTS":True

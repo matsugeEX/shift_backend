8033422a84342a5ca65f35709df0468a173870b0
matsugeEX <matsuge0727@icloud.com>
2026-06-15 12:28:58 +0000

    use production settings module

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,27 @@
 # production.py
 from .base import *
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv(
